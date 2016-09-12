0786cfd1e8d7ff77b832795160475f4158b97aa3
RadkaKalpakova <radka_kalpakova@abv.bg>
2016-09-12 21:10:52 +0300

    mess

--- ajaxbasics/js/script.js
+++ ajaxbasics/js/script.js
@@ -5,19 +5,21 @@ document.addEventListener("DOMContentLoaded",
 		//Unobtrusive event binding
 		document.querySelector("button")
 			.addEventListener("click", function () {
-				//var self = this;
-				//var name = "";
+				
 
 				//Call server to get the name
-				$ajaxUtils.sendGetRequest("/data/name.txt",
+				$ajaxUtils
+				 .sendGetRequest("/data/name.txt",
 						function (request) {
 							var name = request.responseText;
+						
 							
+						document.querySelector("#content")
+							.innerHTML = "<h2>Hello " + name + "!";
+
 							
 						});
-						document.querySelector("#content")
-						.innerHTML = "<h2>Hello " + name + "!";
-					
-			});
+				});
+			
 		}
 	);
